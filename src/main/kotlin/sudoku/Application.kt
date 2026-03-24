e6d6ee5413e87d70d7e9b30d3d6991c99b6c59cb
alexg <alexgsh404@gmail.com>
2026-03-24 20:27:39 +0100

    enforce https

--- src/main/kotlin/sudoku/Application.kt
+++ src/main/kotlin/sudoku/Application.kt
@@ -3,6 +3,7 @@ package sudoku
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.origin
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
@@ -12,6 +13,12 @@ fun main() {
         install(WebSockets)
         routing {
             webSocket("/chat") {
+
+                if (call.request.origin.scheme != "https") {
+                    close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "WSS required"))
+                    return@webSocket
+                }
+
                 send("Please enter your name")
                 for (frame in incoming) {
                     if (frame is Frame.Text) {
